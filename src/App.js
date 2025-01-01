853cf6b38160599dfc8de4bb090a1d5c7b828549
Luis Armando <asdf32easd@gmail.com>
2024-12-31 17:05:07 -0700

    primer commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './styles/css/normalize.css';
+import './styles/css/styles.css';
+
+
+function App() {
+  return (
+    <h1>
+      Café blog
+    </h1>
+  );
+}
+
+export default App;
